8e7664ba5bd448f7af4b408d4662be4d0b5e917d
tamura <tamuten310@gmail.com>
2021-03-24 23:16:13 +0900

    Spring Test 仮実行まで

---
+++ src/test/java/com/example/demo/UserDaoTest.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.login.domain.repository.UserDao;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class UserDaoTest {
+	@Autowired
+	@Qualifier("UserDaoJdbcImpl")
+	UserDao dao;
+
+	@Test
+	public void countTest() {
+		assertEquals(dao.count(), 2);
+	}
+}
